bbbc8c515034a8c8309853a7bccd39b55cf3c81d
John Gorenfeld <john.gorenfeld@gmail.com>
2021-06-14 10:44:56 +0100

    Z-index boxes

--- src/ui/Journey.js
+++ src/ui/Journey.js
@@ -3,11 +3,11 @@ import { getIconForAnswer } from "../helpers/iconUtils";
 export default function Journey(props) {
     const answers = props.answers;
     const JourneyMarks = getJourneyMarks(answers);
-    const JourneyClassName = "journey";
+    const JourneyClassName = "journey icons";
 
     return (
         <div className={JourneyClassName}>
-           <ul className="icons">{JourneyMarks}</ul>
+           {JourneyMarks}
        </div>
     )
 }
@@ -15,8 +15,9 @@ export default function Journey(props) {
 function getJourneyMarks(answers) {
     return answers.map((answer, index) => {
         const icon = getIconForAnswer(answer, index);
+        const zIndex = 100- index;
         return (
-            <li key={index}>{icon}</li>
+            <span key={index} style={{zIndex: zIndex, height: "65px"}}>{icon}</span>
         )
     });
 }
