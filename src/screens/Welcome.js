160b033b5f75efe3218f22edad5e74c519380a36
John Gorenfeld <john@gorenfeld.co.uk>
2021-07-09 10:49:35 +0100

    New logo

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -4,7 +4,7 @@ export default function WelcomeScreen(props) {
 
     return <div className="welcome">
         <img src="/images/numbers/0-on.png" alt="welcome" className="graphicBlock"/>
-        <img src="/images/welcome_logo.png" alt="welcome logo" className="logo"/>
+        <img src="/images/logo_equity.png" alt="welcome logo" className="logo"/>
         <div className="play">
             <a onClick={props.onClickWelcomeButton}><img src="/images/play.png" alt="play button" className="playImage"/></a>
         </div>
